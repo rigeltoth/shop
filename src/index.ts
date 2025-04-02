4df7db333801d3906c13d011ba4ef6617e333d53
Ronnovič Rigel Toth <97006094+rigeltoth@users.noreply.github.com>
2025-04-02 11:20:45 -0500

    X-Forwarded-For error

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,16 @@
 import { bootstrap, runMigrations } from '@vendure/core';
 import { config } from './vendure-config';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
+//runMigrations(config)
+//    .then(() => bootstrap(config))
+//    .catch(err => {
+//        console.log(err);
+//    });
+    
 runMigrations(config)
     .then(() => bootstrap(config))
-    .catch(err => {
-        console.log(err);
-    });
+    .then((app) => { // there may be a more plugin-y way to do this but idk it
+        (app as NestExpressApplication).set('trust proxy', 1);
+    })
+    .catch((err) => { console.error(err); });
